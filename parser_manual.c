a410b6bd044264189129a7c112bdeb5f854d82f1
fendermcbender <55134864+grips1@users.noreply.github.com>
2023-11-23 21:10:34 +0200

    dirty 200 OK
    no cleanup, HTTP GET works :+1:

--- parser_manual.c
+++ parser_manual.c
@@ -8,7 +8,6 @@
 #include <unistd.h>
 #include <netinet/in.h>
 #include <errno.h>
-
 int obtain_src(char* hostname, char* directory)
 {
     //implement DNS lookup to obtain IP of given URL - addr
@@ -16,30 +15,32 @@ int obtain_src(char* hostname, char* directory)
 	int 		sockfd;
 	short int	buffer;
 	char* 		ip;
-	struct addrinfo* 		hints = malloc(sizeof(struct addrinfo));
+	struct addrinfo* 		sock_params = malloc(sizeof(struct addrinfo));
     struct addrinfo* 		result;
 	struct sockaddr_in* 	address_struct;
+	struct sockaddr*		address_struct_caster;
 	struct in_addr			temp_struct;
-	//char 					header[100];
+	char*					response;
 	char*					header;
-	//sprintf(header, "GET %s HTTP/1.1\nHostname: %s\n", directory, hostname); //note:HTTP3 isn't backwards compatible, so this wouldn't work.
-	header = "GET / HTTP/1.1\nHost: httpforever.com\n";
+	unsigned short int		port;
+	port = 80;
+	response = malloc(500);
+	header = "GET / HTTP/1.1\r\nHost: httpforever.com\r\n\r\n";
 	printf("header constructed:\n%s\n", header);
-	printf("voodoo size of header:%d\n", strlen(header));
+	printf("voodoo size of header:%d\nSize of the response buffer:%d\n", strlen(header), sizeof(response));
 	ip = malloc(100 * sizeof(char));
-	memset(hints, 0, sizeof(struct addrinfo));
-	hints->ai_family = AF_INET;
-    hints->ai_socktype = SOCK_STREAM;
-    hints->ai_protocol = 6; //(tcp)
+	memset(sock_params, 0, sizeof(struct addrinfo));
+	sock_params->ai_family = AF_INET;
+    sock_params->ai_socktype = SOCK_STREAM;
+    sock_params->ai_protocol = 6; //(tcp)
 	error = getaddrinfo(hostname, "https", NULL, &result);
 	if(error != 0)// gonna try gethostbyname() before getaddrinfo
     {
 		printf("getaddr failure!\nError code:%d\nQuitting...\n", error);
-		free(hints);
+		free(sock_params);
 		return -1;
 	}
-	free(hints);
-	while(result != NULL)
+	while(result != NULL) //setting result's(of type sockaddr) struct parameters to match those of the server.
 	{
 		if(result->ai_family == AF_INET6)
 		{
@@ -58,9 +59,6 @@ int obtain_src(char* hostname, char* directory)
 	}
 	address_struct = (struct sockaddr_in*) result->ai_addr;
 	temp_struct = address_struct->sin_addr;
-	//int inet_aton(const char *cp, struct in_addr *inp);
-	//printf("ipv4:%s\n", result->ai_addr->sa_data);
-	//const char *inet_ntop(int af, const void *src, char *dst, socklen_t size);
 	if((ip = (inet_ntop(AF_INET, &temp_struct, ip, (2* sizeof(ip))))) == NULL)
     {
         printf("Error converting IP to string\n");
@@ -72,27 +70,29 @@ int obtain_src(char* hostname, char* directory)
 	error = inet_aton(ip, &temp_struct);
 	if(error == 0)
 	{
-		printf("Error converting address\n");
+		printf("Error converting address back to binary, from string.\n");
 		return -1;
 	}
-
 	//connect to server socket
-	if(!(sockfd = socket(AF_INET, SOCK_STREAM, 6)))
+	if(!(sockfd = socket(sock_params->ai_family,sock_params->ai_socktype,sock_params->ai_protocol)))
     {
         printf("Socket File Descriptor error! Quitting..\n");
         close(sockfd);
         return -1;
     }
-	error = connect(sockfd, result->ai_addr, sizeof(struct sockaddr));
+	free(sock_params);
+	address_struct = (struct sockaddr_in*) result->ai_addr;
+	address_struct->sin_port = htons(port);
+	address_struct->sin_family = AF_INET;
+	//	address_struct->sin_addr = (unsigned int) result->ai_addr->sa_data;
+	//address_struct_caster = (struct sockaddr*) address_struct;
+	error = connect(sockfd, address_struct, sizeof(struct sockaddr));
 	if(error != 0)
 	{
 		printf("Connection failure\n");
 		close(sockfd);
 		return -1;
 	}
-    //send HTTP packet, how to craft the header?
-	//snprintf()?
-	//create formatted GET request format. What're the requirements for an HTTP GET request?
 	if(!(buffer = send(sockfd, header, strlen(header), 0)) )
 	{
 		printf("send() failed, error number: %d\n", errno);
@@ -100,8 +100,10 @@ int obtain_src(char* hostname, char* directory)
 		return -1;
 	}
 	printf("buffer printed:%d\n", buffer);//buffer is being printed, but no data is received...?
-    close(sockfd);
+	//read the response, nothing is returned if GET isn't valid
 	//channel response from the server to local directory
+	//read(sockfd, response, strlen(response));
+	close(sockfd);
 	return 0;
 }
 int main(int argc, char* argv[])
