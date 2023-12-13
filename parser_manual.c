e3919565b75ab7fd033fdfeb88eef4086384915d
fendermcbender <55134864+grips1@users.noreply.github.com>
2023-12-13 00:40:03 +0000

    I forgor :(

--- parser_manual.c
+++ parser_manual.c
@@ -8,50 +8,46 @@
 #include <unistd.h>
 #include <netinet/in.h>
 #include <errno.h>
-int obtain_src(char* hostname, char* directory)
+#define PORT 443
+
+FILE* obtain_src(char* hostname, char* directory)
 {
     //implement DNS lookup to obtain IP of given URL - addr
-    int 		error;
+    	FILE*		response_file;
+	int 		error;
 	int 		sockfd;
-	short int	buffer;
+	int			buffer;
 	char* 		ip;
 	struct addrinfo* 		sock_params = malloc(sizeof(struct addrinfo));
-    struct addrinfo* 		result;
+    	struct addrinfo* 		result;
 	struct sockaddr_in* 	address_struct;
-	struct sockaddr*		address_struct_caster;
 	struct in_addr			temp_struct;
 	char*					response;
-	char*					header;
-	unsigned short int		port;
-	port = 80;
-	response = malloc(500);
-	header = "GET / HTTP/1.1\r\nHost: httpforever.com\r\n\r\n";
-	printf("header constructed:\n%s\n", header);
-	printf("voodoo size of header:%d\nSize of the response buffer:%d\n", strlen(header), sizeof(response));
+	char					header[1000];
+	response = malloc(100000);
+	sprintf(header, "GET %s HTTP/1.1\r\nHost: %s\r\n", directory, hostname);
 	ip = malloc(100 * sizeof(char));
 	memset(sock_params, 0, sizeof(struct addrinfo));
 	sock_params->ai_family = AF_INET;
     sock_params->ai_socktype = SOCK_STREAM;
     sock_params->ai_protocol = 6; //(tcp)
 	error = getaddrinfo(hostname, "https", NULL, &result);
-	if(error != 0)// gonna try gethostbyname() before getaddrinfo
+	if(error != 0)
     {
 		printf("getaddr failure!\nError code:%d\nQuitting...\n", error);
 		free(sock_params);
-		return -1;
+		return NULL;
 	}
-	while(result != NULL) //setting result's(of type sockaddr) struct parameters to match those of the server.
+	while(result != NULL) 
 	{
 		if(result->ai_family == AF_INET6)
 		{
 			result = result->ai_next;
-			printf("Received IPv6 address and skipping...\n");
+			printf("IPv6 skipped!\n");
 			continue;
 		}
 		else if(result->ai_family == AF_INET)
 		{
-			//address_struct =  result->ai_addr; //no type casting needed according to man connect()...?
-			printf("IPv4 address found for hostname.\n");
 			break;
 		}
 		else
@@ -64,48 +60,53 @@ int obtain_src(char* hostname, char* directory)
         printf("Error converting IP to string\n");
 		printf("error number:%d\n", errno);
         free(ip);
-		return -1;
+		return NULL;
     }
     printf("IPv4 address:%s\n", ip);
 	error = inet_aton(ip, &temp_struct);
 	if(error == 0)
 	{
 		printf("Error converting address back to binary, from string.\n");
-		return -1;
+		return NULL;
 	}
 	//connect to server socket
 	if(!(sockfd = socket(sock_params->ai_family,sock_params->ai_socktype,sock_params->ai_protocol)))
     {
         printf("Socket File Descriptor error! Quitting..\n");
         close(sockfd);
-        return -1;
+        return NULL;
     }
 	free(sock_params);
 	address_struct = (struct sockaddr_in*) result->ai_addr;
-	address_struct->sin_port = htons(port);
+	address_struct->sin_port = htons(PORT);
 	address_struct->sin_family = AF_INET;
-	//	address_struct->sin_addr = (unsigned int) result->ai_addr->sa_data;
-	//address_struct_caster = (struct sockaddr*) address_struct;
 	error = connect(sockfd, address_struct, sizeof(struct sockaddr));
 	if(error != 0)
 	{
 		printf("Connection failure\n");
 		close(sockfd);
-		return -1;
+		return NULL;
 	}
-	if(!(buffer = send(sockfd, header, strlen(header), 0)) )
+	if(!(buffer = send(sockfd, header, sizeof(header), 0)) )
 	{
-		printf("send() failed, error number: %d\n", errno);
+		printf("GET failure!\n");
 		close(sockfd);
-		return -1;
+		return NULL;
 	}
-	printf("buffer printed:%d\n", buffer);//buffer is being printed, but no data is received...?
-	//read the response, nothing is returned if GET isn't valid
-	//channel response from the server to local directory
-	//read(sockfd, response, strlen(response));
+	printf("data sent:%d\n\n\n", buffer);
+	buffer = read(sockfd, response, (100000 * sizeof(char)) );
+	if(!buffer)
+	{
+		printf("read error! quitting\n");
+		return NULL;
+	}
+	printf("size read:%d\nresponse header:\n\n%s\n", buffer, response);
 	close(sockfd);
-	return 0;
+	response_file = fopen(hostname, "w+");
+	fputs(response, response_file);
+	return response_file;
 }
+
 int main(int argc, char* argv[])
 {
     if(argc < 2)
@@ -113,10 +114,13 @@ int main(int argc, char* argv[])
         printf("Provide URL without the scheme\n");
         return -1;
     }
-	//separate the hostname from the directory assuming no scheme specified
-	char 		hostname[100];// = malloc(100 * sizeof(char));
-	char* 		directory;// = malloc(200 * sizeof(char));
-	int 		i;
+	char 		hostname[100];
+	char* 		directory;
+	int 		i, z;
+	char* 		line_buffer;
+	FILE*		header_file;
+	line_buffer = (char*) malloc(5000);
+	memset(line_buffer, 0, 5000);
 	directory = strchr(argv[1], '/');
 	printf("Directory:%s\n", directory);
 	for(i = 0; i < 100 && (*(argv[1] + i) != '/' && *(argv[1] + i) != '\0'); i++)
@@ -125,13 +129,29 @@ int main(int argc, char* argv[])
 	}
 	hostname[i] = '\0';
 	printf("hostname:%s\n", hostname);
-	if(obtain_src(hostname, directory) != 0)
+
+	if((header_file = obtain_src(hostname, directory)) == NULL)
     {
 		printf("Failure translating hostname, quitting.\n");
 		return -1;
 	}
-	//parse data stored in obtain_src()
-	//free(directory); //after call to obtain_src
-    //free(hostname);
+	fclose(header_file);
+	header_file = fopen(hostname, "r");
+	while((fgets(line_buffer, 5000, header_file)) != NULL)
+	{
+		for(i = 0; i < 200; i++)
+		{
+			if(*(line_buffer+i) == 'h' && *(line_buffer+i+5) == '"') //IF HREF DETECTED
+			{
+				for(z = 6;z < 200; z++) //start from the double quotes
+				{
+					if((*(line_buffer+i+z) != 0 && *(line_buffer+i+z) != '"'))
+						printf("%c", *(line_buffer+i+z));
+					else break;
+				}
+				printf("\n");
+			}
+		}
+	}
 	return 0;
 }
